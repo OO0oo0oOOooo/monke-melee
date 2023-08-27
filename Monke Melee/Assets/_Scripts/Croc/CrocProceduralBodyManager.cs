6c1ff5cc19edd6468c60001040786cc53efc7d18
Kade <97352407+OO0oo0oOOooo@users.noreply.github.com>
2023-08-26 20:21:29 -0600

    Croc body to surface normal tests

--- Monke Melee/Assets/_Scripts/Croc/CrocProceduralBodyManager.cs
+++ Monke Melee/Assets/_Scripts/Croc/CrocProceduralBodyManager.cs
@@ -1,10 +1,49 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CrocProceduralBodyManager : MonoBehaviour
 {
-    // Get all the bones in the spine
-    // Set up IK chain spine
+    public List<Transform> spineBones;
+    [SerializeField] private LayerMask _layerMask;
 
-    // Raycast to the local -y direction from each ik target
-    // rotate bone ik target to match normal of hit
+    // Raycast to the local z direction from each bone
+    // rotate bone to match normal of hit
+
+    // private void Start()
+    // {
+    //     spineBones = new List<Transform>();
+    //     Transform[] children = GetComponentsInChildren<Transform>();
+    //     foreach (Transform child in children)
+    //     {
+    //         if (child.name.Contains("Spine"))
+    //         {
+    //             spineBones.Add(child);
+    //         }
+    //     }
+    // }
+
+    // Clamp bones to a certain angle
+    // Interpolate Rotation
+    // Lerp to default rotation when hit is null
+
+    private void Update()
+    {
+        foreach (Transform bone in spineBones)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(bone.position, bone.forward, out hit, 1f, _layerMask))
+            {
+                bone.rotation = Quaternion.LookRotation(hit.normal, bone.up);
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        foreach (Transform bone in spineBones)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(bone.position, bone.forward);
+        }
+    }
 }
