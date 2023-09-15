0d9f02634755231691a3a55eaea9275170cbd0ee
Kade <97352407+OO0oo0oOOooo@users.noreply.github.com>
2023-09-14 18:32:10 -0600

    body animation

--- Monke Melee/Assets/_Scripts/Croc/CrocBody.cs
+++ Monke Melee/Assets/_Scripts/Croc/CrocBody.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class CrocBody : MonoBehaviour
 {
+    private List<Vector3> ControlPoints = new List<Vector3>();
+    public List<Transform> Segments;
+
+    [SerializeField] private float distanceBetweenControlPoints = 0.8f;
+
+    [SerializeField] private float segmentOffset = 1f;
+
     // list of segments
     // Segments follow each other like a snake
     // segments raycasy down to get the normal of the ground
@@ -14,19 +21,10 @@ public class CrocBody : MonoBehaviour
     // create a list of positions for the segments to follow
     // the lead segment will place a 
 
-    public List<Vector3> ControlPoints;
-    
-
-
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        ControlPoints.Add(Segments[0].position);
+
     }
 }
